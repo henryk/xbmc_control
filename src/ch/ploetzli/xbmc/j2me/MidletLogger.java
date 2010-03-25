822e72e4651fd52794711048bcbafbd6a785993e
Henryk Plötz <henryk@ploetzli.ch>
2010-03-25 11:24:24 +0100

    Get rid of warning

--- src/ch/ploetzli/xbmc/j2me/MidletLogger.java
+++ src/ch/ploetzli/xbmc/j2me/MidletLogger.java
@@ -7,11 +7,9 @@ import javax.microedition.midlet.MIDlet;
 import ch.ploetzli.xbmc.Logger;
 
 public class MidletLogger extends Logger {
-	private MIDlet midlet;
 	private Vector entries = new Vector();
 
 	public MidletLogger(MIDlet midlet) {
-		this.midlet = midlet;
 	}
 
 	public synchronized void info(String s) {
